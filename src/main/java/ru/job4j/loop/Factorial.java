b7f383a67f1b551abcd24aedf10b6cf52f5fd876
ASVidin <Andrey.Vidin@gmail.com>
2021-12-06 23:19:26 +0300

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.Locale;
+
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
@@ -7,5 +9,20 @@ public class Factorial {
             result *= i;
         }
         return result;
+    }
+
+    public static String collect(String s) {
+        StringBuilder result = new StringBuilder(String.valueOf(s.charAt(0)));
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                result.append(s.charAt(i + 1));
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(collect("qwe garrqw rth").toUpperCase(Locale.ROOT));
     }
 }
